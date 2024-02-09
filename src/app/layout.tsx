c5c5b07eee106a37ff2bf373a0ef6de7a56e71ad
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-09 15:28:31 -0700

    Build failing on Vercel, could be this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
