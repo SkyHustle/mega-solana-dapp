161444c9668c8f6a9208531cca46fe0cb3571e57
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-10 15:42:01 -0700

    Update homepage header

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -1,9 +1,9 @@
 export default function DashboardFeature() {
   return (
-    <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
-      <div className="space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Welcome to Mega Solana Dapp</h2>
-        <p className="text-lg text-gray-500">Solana Dapp Playground</p>
+    <div className="bg-background px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-bold tracking-tight text-primary sm:text-6xl">Welcome to Mega Solana Dapp</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-500">Solana Dapp Playground</p>
       </div>
     </div>
   );
