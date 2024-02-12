a19b30d38377385f539bffcaa3a459b3854d3de6
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-12 12:03:04 -0700

    Account details boilerplate

--- src/components/account/account-details-feature.tsx
+++ src/components/account/account-details-feature.tsx
@@ -1,3 +1,66 @@
+"use client";
+
+import { PublicKey } from "@solana/web3.js";
+import { useMemo } from "react";
+import { useParams } from "next/navigation";
+
 export default function AccountDetailFeature() {
-  return <div>Account Details</div>;
+  const params = useParams();
+
+  const address = useMemo(() => {
+    if (!params.address) {
+      return;
+    }
+    try {
+      return new PublicKey(params.address);
+    } catch (e) {
+      console.log(`Invalid public key`, e);
+    }
+  }, [params]);
+  if (!address) {
+    return <div>Error loading account</div>;
+  }
+
+  return (
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-5 sm:py-8 lg:py-9">
+        <div className="text-center">
+          <h1 className="text-1xl font-bold tracking-tight text-primary sm:text-2xl">SOL Balance: 4.567</h1>
+          <p className="mt-2 leading-8 text-gray-500">Wallet Address</p>
+          <div className="mt-4 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Airdrop
+            </a>
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Send
+            </a>
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Receive
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
