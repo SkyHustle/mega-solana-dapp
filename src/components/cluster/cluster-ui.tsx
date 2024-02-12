404fa36c634d077bda0a9508a1d4a2fd2297818c
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-11 19:23:43 -0700

    Phase 1 cluster table build out

--- src/components/cluster/cluster-ui.tsx
+++ src/components/cluster/cluster-ui.tsx
@@ -9,6 +9,7 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Trash2 } from "lucide-react";
 
 type Checked = DropdownMenuCheckboxItemProps["checked"];
 
@@ -34,5 +35,106 @@ export function ClusterUiSelect() {
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+export function ClusterUiTable() {
+  const { clusters, setCluster, deleteCluster } = useCluster();
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table border-4 border-separate border-base-300">
+        <thead>
+          <tr>
+            <th>Name/ Network / Endpoint</th>
+            <th className="text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clusters.map((item) => (
+            <tr key={item.name} className={item?.active ? "bg-base-200" : ""}>
+              <td className="space-y-2">
+                <div className="whitespace-nowrap space-x-2">
+                  <span className="text-xl">
+                    {item?.active ? (
+                      item.name
+                    ) : (
+                      <button title="Select cluster" className="link link-secondary" onClick={() => setCluster(item)}>
+                        {item.name}
+                      </button>
+                    )}
+                  </span>
+                </div>
+                <span className="text-xs">Network: {item.network ?? "custom"}</span>
+                <div className="whitespace-nowrap text-gray-500 text-xs">{item.endpoint}</div>
+              </td>
+              <td className="space-x-2 whitespace-nowrap text-center">
+                <button
+                  disabled={item?.active}
+                  className="btn btn-xs btn-default btn-outline"
+                  onClick={() => {
+                    if (!window.confirm("Are you sure?")) return;
+                    deleteCluster(item);
+                  }}
+                >
+                  IconTrash
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function TableDemo() {
+  const { clusters, setCluster, deleteCluster } = useCluster();
+  console.log("clusters", clusters);
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Network</TableHead>
+          <TableHead>Endpoint</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {clusters.map((item) => (
+          <TableRow key={item.name} className={item?.active ? "bg-secondary" : ""}>
+            <TableCell className="font-medium">
+              <span className="text-lg">
+                {item?.active ? (
+                  item.name
+                ) : (
+                  <button title="Select cluster" className="link link-secondary" onClick={() => setCluster(item)}>
+                    {item.name}
+                  </button>
+                )}
+              </span>
+            </TableCell>
+            <TableCell>{item.network ?? "custom"}</TableCell>
+            <TableCell>{item.endpoint}</TableCell>
+            <TableCell className="text-right">
+              <Trash2 className="h-6 w-6 float-end" />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
