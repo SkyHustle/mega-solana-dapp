d5b1132f407ba6409fd4257c1a10f7f186c0c41e
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-12 15:03:02 -0700

    Getting some really slow FCP and LCP scores using Vercerl's speed insight, wondering if it's because of this suspense boundary

--- src/components/ui/ui-layout.tsx
+++ src/components/ui/ui-layout.tsx
@@ -6,16 +6,7 @@ export function UiLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-full flex flex-col">
       <NavBar />
-
-      <Suspense
-        fallback={
-          <div className="text-center my-32">
-            <span className="loading loading-spinner loading-lg"></span>
-          </div>
-        }
-      >
-        {children}
-      </Suspense>
+      {children}
     </div>
   );
 }
