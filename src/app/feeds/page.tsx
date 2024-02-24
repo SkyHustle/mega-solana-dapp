ed4f9a8e61476addba67ed9fac3f210f5cf597a4
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-24 15:39:30 -0700

    Hard code feed table with expected data

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -4,6 +4,62 @@ import { OCR2Feed } from "@chainlink/solana-sdk";
 import { PublicKey } from "@solana/web3.js";
 import { AnchorProvider } from "@project-serum/anchor";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const feeds = [
+  {
+    iconURL: "/solana.svg",
+    symbol: "SOL",
+    name: "Solana",
+    price: "$110.00",
+    feedAddress: "99B2bTijsU6f1GCT73HmdR7HCFFjGMBcPZY6jZ96ynrR",
+  },
+  {
+    iconURL: "/bitcoin.svg",
+    symbol: "BTC",
+    name: "Bitcoin",
+    price: "$50,000",
+    feedAddress: "6PxBx93S8x3tno1TsFZwT5VqP8drrRCbCXygEXYNkFJe",
+  },
+  {
+    iconURL: "/ethereum.svg",
+    symbol: "ETH",
+    name: "Ethereum",
+    price: "$3,000",
+    feedAddress: "669U43LNHx7LsVj95uYksnhXUfWKDsdzVqev3V4Jpw3P",
+  },
+  {
+    iconURL: "/chainlink.svg",
+    symbol: "LINK",
+    name: "Chainlink",
+    price: "$20",
+    feedAddress: "HXoZZBWv25N4fm2vfSKnHXTeDJ31qaAcWZe3ZKeM6dQv",
+  },
+  {
+    iconURL: "/usdc.svg",
+    symbol: "USDC",
+    name: "USDC",
+    price: "$1",
+    feedAddress: "2EmfL3MqL3YHABudGNmajjCpR13NNEn9Y4LWxbDm6SwR",
+  },
+  {
+    iconURL: "/tether.svg",
+    symbol: "USDT",
+    name: "Tether",
+    price: "$1",
+    feedAddress: "8QQSUPtdRTboa4bKyMftVNRfGFsB4Vp9d7r39hGKi53e",
+  },
+];
+
 // Note that the Wallet object provided by the useWallet hook from @solana/wallet-adapter-react
 // is not compatible with the Wallet object that the Anchor Provider expects
 import { useConnection, useAnchorWallet } from "@solana/wallet-adapter-react";
@@ -56,7 +112,29 @@ const ChainlinkPriceFeed = () => {
     };
   }, [wallet, connection]);
 
-  return <div>{price ? `Live SOL Price: $${price}` : "Loading..."}</div>;
+  return (
+    <Table className="mt-16">
+      <TableCaption>A List of Chainlink Price Feeds</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Icon</TableHead>
+          <TableHead>Symbol</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead className="text-right">Price</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {feeds.map((feed) => (
+          <TableRow key={feed.name}>
+            <TableCell className="font-medium">{feed.iconURL}</TableCell>
+            <TableCell>{feed.symbol}</TableCell>
+            <TableCell>{feed.name}</TableCell>
+            <TableCell className="text-right">{feed.price}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default ChainlinkPriceFeed;
