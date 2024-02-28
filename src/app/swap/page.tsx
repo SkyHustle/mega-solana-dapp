041d743dbce36d00ebab637fff07bdd3607b2e8a
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-27 17:11:39 -0700

    Make sure Swap is only available when the cluster is on mainnet

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -2,9 +2,12 @@
 import Script from "next/script";
 import { useEffect, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { useCluster } from "@/components/cluster/cluster-data-access";
 
 export default function JupiterSwap() {
   const [isJupiterLoaded, setJupiterLoaded] = useState(false);
+  const { cluster } = useCluster();
+  console.log("cluster name", cluster.name);
 
   const passthroughWalletContextState = useWallet();
 
@@ -25,31 +28,44 @@ export default function JupiterSwap() {
   };
 
   useEffect(() => {
-    // Call initializeJupiter if Jupiter hasn't been loaded yet
-    if (!isJupiterLoaded) {
+    if (cluster.name === "mainnet") {
       initializeJupiter();
     }
 
     if (window.Jupiter && window.Jupiter.syncProps) {
       window.Jupiter.syncProps({ passthroughWalletContextState });
     }
-  }, [isJupiterLoaded, passthroughWalletContextState]);
+  }, [isJupiterLoaded, passthroughWalletContextState, cluster]);
 
   return (
     <div className="flex justify-center items-center sm:mt-24">
-      <Script
-        src="https://terminal.jup.ag/main-v2.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          // Call the initialize function once the script is loaded
-          if (!isJupiterLoaded) {
-            initializeJupiter();
-          }
-        }}
-      />
-      <div className="bg-black sm:w-1/2 rounded-md" id="integrated-terminal">
-        {/* Jupiter Terminal should be initialized here */}
-      </div>
+      {cluster.name === "mainnet" ? (
+        <div key={cluster.name} className="">
+          <Script
+            src="https://terminal.jup.ag/main-v2.js"
+            strategy="afterInteractive"
+            onLoad={() => {
+              // Call the initialize function once the script is loaded
+              if (!isJupiterLoaded) {
+                initializeJupiter();
+              }
+            }}
+          />
+
+          <div className="bg-black rounded-md" id="integrated-terminal">
+            {/* Jupiter Terminal should be initialized here */}
+          </div>
+        </div>
+      ) : (
+        <div className="bg-background px-6 py-24 sm:py-32 lg:px-8">
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-4xl font-bold tracking-tight text-primary sm:text-6xl">Swap Only Works On Mainnet</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-500">
+              Make sure your wallet is not in testnet mode and change cluster to mainnet
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
