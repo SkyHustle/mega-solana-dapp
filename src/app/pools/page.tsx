d826882619dd0854dd878804009c530a4f1a7df8
Dmitry Sky <dmitry.sky@icloud.com>
2024-03-16 15:48:39 -0600

    Let's try this again

--- src/app/pools/page.tsx
+++ src/app/pools/page.tsx
@@ -1,7 +1,18 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { nativeToUi } from "@mrgnlabs/mrgn-common";
+import {
+  Balance,
+  Bank,
+  MarginRequirementType,
+  MarginfiAccount,
+  MarginfiClient,
+  PriceBias,
+  RiskTier,
+  getConfig,
+} from "@mrgnlabs/marginfi-client-v2";
+// import { Infer, array, assert, object, string } from "superstruct";
+import { useConnection } from "@solana/wallet-adapter-react";
+
 export default function Pools() {
-  return (
-    <div>
-      <h1>Welcome to MarginFi Lending Pools</h1>
-    </div>
-  );
+  return <div>Pools</div>;
 }
